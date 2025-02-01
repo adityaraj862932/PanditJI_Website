a14e0e7de932632a94cba87268c2485914bbaab4
Krishiv <golusinghrawat712@gmail.com>
2025-02-01 07:49:30 +0530

    second commit

--- Panditji/src/components/Hero.jsx
+++ Panditji/src/components/Hero.jsx
@@ -4,9 +4,9 @@ function Hero() {
   return (
     <div className='w-full h-[88vh] relative '>
         <div className="text absolute right-[6vw] top-[14vh] text-center">
-            <h1 className='text-6xl font-semibold text-orange-500'>A Home For Hindu Devotees</h1>
+            <h1 className='text-6xl font-semibold text-orange-500 font-[Marcellus]'>A Home For Hindu Devotees</h1>
             <h1 className='mt-5 text-2xl'>वैदिक संस्कृति वैदिक संस्कार</h1>
-        <p className='text-3xl mt-2'>Bringing the essence of <br />
+        <p className='text-3xl mt-2 font-[SairaCondensed]'>Bringing the essence of <br />
         ancient Vedic traditions to your doorstep.</p>
         </div>
         
