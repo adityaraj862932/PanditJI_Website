23c251047506d08c9aa66207ad36ff982ac7e891
Sanjan pandit <sanjansonu8674@gmail.com>
2025-02-06 18:40:10 +0530

    Navbar correction with some space and add hover effect and in photos.json add photo

--- Panditji/src/components/Gallery.jsx
+++ Panditji/src/components/Gallery.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import Photos from './Photos'
 function Gallery() {
   return (
-    <div className='h-screen w-full bg-orange-100 p-10 flex'>
-        <div className='h-[300px] w-[200px] bg-orange-500' key={index}></div>
-        { 
-            Photos.map((photo,index)=>(
-                <div className='h-[300px] w-[200px] bg-orange-500' key={index}>
-                <img src={photo.photo} alt="" />
+    <div className='h-screen w-full bg-orange-100 p-10 grid grid-cols-4 gap-4'>
+      {/* <div className='h-[300px] w-[200px] bg-orange-500' key={index}></div> */}
+      {
+        Photos.map((photo, index) => (
+          // <div className='grid grid-cols-4 gap-4'>
+            <div className='h-[300px] w-[300px]' key={index}>
+              <img src={photo.photo} alt="" />
             </div>
-            ))
-           
-        }
+          // </div>
+        ))
 
-</div>
-    
+      }
+
+    </div>
+
   )
 }
 
