58682f1d1631ddfb98c629666954e68f99019b1a
adityaraj862932 <raj862932@gmail.com>
2025-02-06 22:41:08 +0530

    change in gallery

--- Panditji/src/components/Gallery.jsx
+++ Panditji/src/components/Gallery.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import Photos from './Photos'
 function Gallery() {
   return (
-    <div className='h-screen w-full bg-orange-100 p-10 flex'>
-        <div className='h-[300px] w-[200px] bg-orange-500' key={index}></div>
+    <div className='h-screen w-screen bg-orange-200  p-10 flex flex-col overflow-x-hidden'>
+        <div className='w-full h-[15%] flex items-center justify-center text-5xl '><strong >My Work</strong></div>
+        <div className='h-[2px] w-full bg-black'></div>
+        <div className=' flex justify-center  pt-5 gap-4 overflow-y-auto w-full h-[80%] flex-wrap'>
         { 
             Photos.map((photo,index)=>(
-                <div className='h-[300px] w-[200px] bg-orange-500' key={index}>
-                <img src={photo.photo} alt="" />
+                <div className='h-[200px] w-[19%] rounded-xl '>
+                <img className=' rounded-xl h-[200px] w-full' src={photo.photo} alt="" />
             </div>
             ))
            
-        }
+        }</div>
 
 </div>
     
