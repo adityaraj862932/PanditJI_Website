aad7e7ce7e04507bda36e963c20b7c1426401d43
Sanjan pandit <sanjansonu8674@gmail.com>
2025-03-20 15:50:58 +0530

    index.js added

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db"); // Import the database connection file
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+connectDB();
+
+// Routes
+const userRoutes = require("./routes/userRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+
+app.use("/api/users", userRoutes);
+app.use("/api/bookings", bookingRoutes);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
