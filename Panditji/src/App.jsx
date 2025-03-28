3c8ab6a6f21f88286dcc0ec74fa0959cbc1e5557
Sanjan pandit <sanjansonu8674@gmail.com>
2025-03-28 14:32:04 +0530

    kuch v

--- Panditji/src/App.jsx
+++ Panditji/src/App.jsx
@@ -1,15 +1,20 @@
 
 import UserRouting from './components/Users/UserRouting';
 import AdminDashboard from './components/Admin/Admindashboard';
+import { Route, Router, Routes } from 'react-router-dom';
 
 
 function App() {
 
   return (
-    <div>
-      <UserRouting/>
-      {/* <AdminDashboard/> */}
-    </div>
+    // <div>
+    //   <UserRouting/>
+    //   {/* <AdminDashboard/> */}
+    // </div>
+
+      <Routes>
+        <Route path='/admin' element={<AdminDashboard/>} />
+      </Routes>
   );
 }
 
